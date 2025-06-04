ad0e36db5872f3e49c6116a2b90551a86f790e2e
DOODLE73 <116104482+DOODLE73@users.noreply.github.com>
2025-06-04 20:39:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
-const path = require("path");
+const path = require('path');
 
 
 const app = express();
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 // app.use(express.static(path.join(__dirname, "../frontend/build")));
 
@@ -47,12 +51,6 @@ app.post('/api/contact', async (req, res) => {
     console.error('Email sending failed:', error);
     res.status(500).send('Email failed!');
   }
-});
-
-
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 app.listen(PORT, () => {
