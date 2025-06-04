edef1ff9afe71f85d8d8fa696fa4608e5ea867e5
DOODLE73 <116104482+DOODLE73@users.noreply.github.com>
2025-06-04 20:10:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
