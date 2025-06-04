b3b018d6099e0204cc24fa9c4691ee710feb65b1
DOODLE73 <116104482+DOODLE73@users.noreply.github.com>
2025-06-04 20:22:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.post('/api/contact', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/*", (req, res) => {
+app.get("/:path(*)", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
